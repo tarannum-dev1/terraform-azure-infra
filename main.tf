ba19cd6c6a43482723f19060965249dec3e3efa7
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-26 01:37:02 +0530

    Add subnet resource to main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,13 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = var.vnet_address_space
 }
+#################################################
+# Subnet
+#################################################
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefixes
+}
