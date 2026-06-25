3762ad5e01227e106ef9991c91c5bbddfc79f661
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-26 00:59:22 +0530

    Add variables for resource group deployment

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
+# Azure Region
+variable "location" {
+  description = "Azure region where resources will be created."
+  type        = string
+  default     = "Central India"
+}
 
+# Resource Group Name
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group."
+  type        = string
+  default     = "rg-contoso-dev"
+}
