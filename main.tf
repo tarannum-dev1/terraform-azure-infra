5ecd69c7973e6f08bf0b295916d05c6b019a39b9
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-26 01:24:05 +0530

    Add Azure Virtual Network resource to main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+#################################################
+# Virtual Network
+#################################################
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.vnet_address_space
+}
