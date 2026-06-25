71a6eac5ad374724ba3910227ff18e2d20e981eb
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-26 01:22:38 +0530

    Add Virtual Network variables to variables.tf
    Added variables for Virtual Network configuration.

--- variables.tf
+++ variables.tf
@@ -15,3 +15,16 @@ variable "resource_group_name" {
   description = "Name of the Azure Resource Group."
   type        = string
 }
+#################################################
+# Virtual Network
+#################################################
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network."
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the Virtual Network."
+  type        = list(string)
+}
