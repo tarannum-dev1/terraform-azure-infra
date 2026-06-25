bcbf5f4e970cc2d5456c2c1c961b7a9e52ec5a1f
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-26 01:09:59 +0530

    Add Terraform outputs
    Add outputs for resource group name and location.

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,13 @@
+#################################################
+# Outputs
+#################################################
 
+output "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "resource_group_location" {
+  description = "Location of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.location
+}
