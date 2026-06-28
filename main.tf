d9a6fa45b22ee1d9149595b9d37c3655dba5d532
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-28 20:47:33 +0530

    Add Linux virtual machine resource configuration
    Added a new resource for a Linux virtual machine with SSH authentication and specified OS disk settings.

--- main.tf
+++ main.tf
@@ -71,3 +71,37 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
+#################################################
+# Linux Virtual Machine
+#################################################
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
