f2ff5efb7b8f557acfab91dd6f1790c089473507
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-28 20:39:02 +0530

    Add Azure Network Interface resource configuration

--- main.tf
+++ main.tf
@@ -55,3 +55,19 @@ resource "azurerm_public_ip" "public_ip" {
   allocation_method = "Static"
   sku               = "Standard"
 }
+#################################################
+# Network Interface
+#################################################
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
