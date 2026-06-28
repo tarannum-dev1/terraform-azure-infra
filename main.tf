c9fa4cba2d790d1d5dfc7ffe66d00ba0bba4b68c
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-28 20:06:01 +0530

    Add resource for static public IP allocation

--- main.tf
+++ main.tf
@@ -43,3 +43,15 @@ resource "azurerm_subnet_network_security_group_association" "web_nsg_associatio
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.web_nsg.id
 }
+#################################################
+# Public IP
+#################################################
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
