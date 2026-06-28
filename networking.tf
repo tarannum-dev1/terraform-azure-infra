ef9e2defc67cc5e99d86b6981d9223a73ace14c1
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-29 00:24:18 +0530

    Refactor Terraform configuration into logical files

--- networking.tf
+++ networking.tf
@@ -1,1 +1,65 @@
+#################################################
+# Virtual Network
+#################################################
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.vnet_address_space
+}
+#################################################
+# Subnet
+#################################################
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefixes
+}
+#################################################
+# Network Security Group
+#################################################
+
+resource "azurerm_network_security_group" "web_nsg" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+#################################################
+# Associate NSG with Subnet
+#################################################
+
+resource "azurerm_subnet_network_security_group_association" "web_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.web_nsg.id
+}
+#################################################
+# Public IP
+#################################################
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+#################################################
+# Network Interface
+#################################################
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
