bb6145a7b19d5dfc4c3602218e0a7f1b799b142a
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-28 20:03:09 +0530

    Add NSG association to subnet in main.tf

--- main.tf
+++ main.tf
@@ -35,3 +35,11 @@ resource "azurerm_network_security_group" "web_nsg" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+#################################################
+# Associate NSG with Subnet
+#################################################
+
+resource "azurerm_subnet_network_security_group_association" "web_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.web_nsg.id
+}
