3342115f86b42ceea97da82c894915fb6d04acf8
Tarannum Ansari <tarannum.ansariy@gmail.com>
2026-06-29 00:25:54 +0530

    Refactor Terraform configuration into logical files

--- compute.tf
+++ compute.tf
@@ -1,1 +1,34 @@
+#################################################
+# Linux Virtual Machine
+#################################################
 
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
